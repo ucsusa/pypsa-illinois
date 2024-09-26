8763aca4ff79d1661de4f6bbee09e6be45134327
Lee Shaver <lshaver@ucsusa.org>
2024-09-26 11:03:32 -0500

    PEP8 fixes

--- sensitivity-script.py
+++ sensitivity-script.py
@@ -12,14 +12,15 @@ shutil.copyfile(base_config_file, backup_config_file)
 
 # Define the variables and their possible values
 variable_ranges = {
-    'fuel_cost_year': [2018,2019,2020,2021,2022,2023],
+    'fuel_cost_year': [2018, 2019, 2020, 2021, 2022, 2023],
     'load_growth': [0, 1],
     'total_demand': [136e6, 185e6],
-    'atb_scenario': ['Moderate'] # 'Conservative', 'Advanced'
+    'atb_scenario': ['Moderate']  # 'Conservative', 'Advanced'
 }
 
 # Create a string of all the rules that must be forced to run
-forcerun = "retrieve_fuel_costs add_electricity" # add retrieve_costs when also modifying atb scenarios
+# NOTE: add retrieve_costs when also modifying atb scenarios
+forcerun = "retrieve_fuel_costs add_electricity"
 
 # Generate all possible combinations of the variables
 variable_combinations = list(itertools.product(
@@ -29,13 +30,20 @@ variable_combinations = list(itertools.product(
     variable_ranges['atb_scenario']
 ))
 
-# ATB scenarios must be handled differently because sub-parameters can't be passed to snakemake in the command line
-atb_scenario_targets = ["scenario: \'Moderate\'", "scenario: \'Conservative\'", "scenario: \'Advanced\'"]
+# ATB scenarios must be handled differently because sub-parameters can't be
+# passed to snakemake in the command line
+atb_scenario_targets = [
+    "scenario: \'Moderate\'",
+    "scenario: \'Conservative\'",
+    "scenario: \'Advanced\'"
+]
+
 
 def replace_strings_in_config(file_path, target_strings, new_value):
-"""
-This function to modifies the `atb_params` in the `config.yml` file (from ChatGPT).
-"""
+    """
+    This function looks for a list of target strings and replaces them with a
+    new value (from ChatGPT).
+    """
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
 
@@ -45,18 +53,39 @@ This function to modifies the `atb_params` in the `config.yml` file (from ChatGP
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(updated_content)
 
+
 run_no = 0
-for idx, (fuel_cost_year, load_growth, total_demand, atb_scenario) in enumerate(variable_combinations):
+for idx, (fuel_cost_year,
+          load_growth,
+          total_demand,
+          atb_scenario) in enumerate(variable_combinations):
+
     # Name the scenario
-    scenario = f"cost-{fuel_cost_year}_growth-{load_growth}_demand-{total_demand:.2E}_atb-{atb_scenario}"
+    scenario = (
+        f"cost-{fuel_cost_year}_"
+        f"growth-{load_growth}_"
+        f"demand-{total_demand:.2E}_"
+        f"atb-{atb_scenario}"
+    )
 
     # Combine all the top-line config variables
-    config = f"time_res=1 fuel_cost_year={fuel_cost_year} load_growth={load_growth} total_demand={total_demand} scenario={scenario}"
+    config = (
+        f"time_res=1 "
+        f"fuel_cost_year={fuel_cost_year} "
+        f"load_growth={load_growth} "
+        f"total_demand={total_demand} "
+        f"scenario={scenario}"
+    )
 
-    # Modify the config file to update the ATB scenario. 
-    # This is disabled because the model won't run -- alternate scenarios don't exist for nuclear
-    #atb_scenario_text = f"scenario: \'{atb_scenario}\'"
-    #replace_strings_in_config(base_config_file, atb_scenario_targets, atb_scenario_text)
+    # Modify the config file to update the ATB scenario.
+    # This is disabled because the model won't run --
+    # alternate scenarios don't exist for nuclear
+    # atb_scenario_text = f"scenario: \'{atb_scenario}\'"
+    # replace_strings_in_config(
+    #    base_config_file,
+    #    atb_scenario_targets,
+    #    atb_scenario_text
+    # )
 
     # Run Snakemake, passing the new variables as configs
     print(f"Running Snakemake with config {run_no} and the following command:")
@@ -65,7 +94,7 @@ for idx, (fuel_cost_year, load_growth, total_demand, atb_scenario) in enumerate(
     run_no += 1
 
     # for debugging:
-    #if run_no == 1: break
+    # if run_no == 1: break
 
 # Restore the original base config after running all scenarios
 shutil.copyfile(backup_config_file, base_config_file)

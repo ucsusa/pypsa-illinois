93e8a240a4bf5a817dcaafbc3f4c021e905540d4
Sam Dotson <sdotson@ucsusa.org>
2024-10-18 09:09:16 -0400

    updates the sensitivity script

--- scripts/nuclear_sensitivity.py
+++ scripts/nuclear_sensitivity.py
@@ -15,11 +15,10 @@ if __name__ == "__main__":
     n = pypsa.Network("../pypsa-illinois/results/advanced_nuclear_v1.1/networks/illinois_solved.nc")
     
     delta = 0.25
-    costs = np.arange(1, 3+delta, delta)
+    costs = np.arange(1.5, 3+delta, delta)
     
     smr_cost_2030 = n.generators.loc[n.generators.index.str.contains('SMR'), 'capital_cost'].unique()[0]
-    
-    results = []
+
     for cost in tqdm(costs):
         n_test = n.copy()
         n_test.generators.loc[n_test.generators.index.str.contains('SMR'), 'capital_cost'] = smr_cost_2030*cost
@@ -27,8 +26,6 @@ if __name__ == "__main__":
         n_test.optimize(solver_name='cplex')
         
         scenario = f"cost-2023_growth_0.01_demand-1.36E+08_atb-Moderate-X-{cost}_v1.1"
-        
-        results.append(n_test)
         
         network_folder = results_path/scenario
         network_folder.mkdir(exist_ok=True)
